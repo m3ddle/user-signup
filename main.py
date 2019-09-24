34d7e5b2dbb80e434ac37af59d5c710a290198c2
Tristen <TristenS@protonmail.com>
2019-09-23 20:46:00 -0400

    Add routing for the welcome page.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+# TODO: Restructure html/page layout. Centered box with proper divs no html tables.
+# TODO: Make pages into proper html template structure
+# TODO: Add error handling for improper/blank fields
+
 from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
+
 @app.route("/")
 def index():
 
     return render_template("index.html")
+
+
+@app.route("/welcome", methods=["POST"])
+def welcome():
+    username = request.form["username"]
+    return render_template("welcome.html", username=username)
+
 
 app.run()
